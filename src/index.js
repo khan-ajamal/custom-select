b895ce8c6f2743172a335405e22c368843759aff
Ajamal Khan <13559558+khan-ajamal@users.noreply.github.com>
2021-04-09 22:46:53 +0530

    feat: initial commit for alpha release
    Changed final distribution filename has being changed from main.js to custom-select.js
    
    BREAKING CHANGE: final distribution filename has being changed

--- src/index.js
+++ src/index.js
@@ -1,124 +1,126 @@
 class CustomSelect {
-  constructor(selector) {
-    const elements = document.querySelectorAll(selector);
-    [...elements].forEach(this.render);
-  }
+    constructor(selector) {
+        const elements = document.querySelectorAll(selector);
+        [...elements].forEach(this.render);
+    }
 
-  createItems = (option) => {
-    if (!option.value) return;
+    createItems = (option) => {
+        if (!option.value) return;
 
-    const item = document.createElement("li");
-    item.dataset.value = option.value;
+        const item = document.createElement("li");
+        item.dataset.value = option.value;
 
-    const input = document.createElement("input");
-    input.setAttribute("type", "checkbox");
-    input.setAttribute("value", option.value);
+        const input = document.createElement("input");
+        input.setAttribute("type", "checkbox");
+        input.setAttribute("value", option.value);
 
-    input.checked = option.selected;
+        input.checked = option.selected;
 
-    item.append(input);
+        item.append(input);
 
-    const span = document.createElement("span");
-    span.innerText = option.text;
+        const span = document.createElement("span");
+        span.innerText = option.text;
 
-    item.append(span);
+        item.append(span);
 
-    return item;
-  };
+        return item;
+    };
 
-  onClickItem = (e, item, isMultiple) => {
-    e.stopPropagation();
+    onClickItem = (e, item, isMultiple) => {
+        e.stopPropagation();
 
-    const inputs = item.parentNode.querySelectorAll("input");
-    const itemInput = item.querySelector("input");
+        const inputs = item.parentNode.querySelectorAll("input");
+        const itemInput = item.querySelector("input");
 
-    if (isMultiple) {
-      if (e.target.nodeName == "INPUT") {
-        return;
-      }
-      itemInput.checked = !itemInput.checked;
-    } else {
-      inputs.forEach((i) => {
-        if (itemInput !== i) {
-          i.checked = false;
+        if (isMultiple) {
+            if (e.target.nodeName == "INPUT") {
+                return;
+            }
+            itemInput.checked = !itemInput.checked;
+        } else {
+            inputs.forEach((i) => {
+                if (itemInput !== i) {
+                    i.checked = false;
+                }
+            });
+            if (e.target.nodeName == "INPUT") {
+                return;
+            }
+            itemInput.checked = !itemInput.checked;
         }
-      });
-      if (e.target.nodeName == "INPUT") {
-        return;
-      }
-      itemInput.checked = !itemInput.checked;
-    }
-  };
+    };
 
-  populateValues = (elem) => {
-    const selectedOptions = elem.parentNode.querySelectorAll("input:checked");
-    const selectedValues = [];
-    for (let o of selectedOptions.values()) {
-      selectedValues.push(o.value);
-    }
+    populateValues = (elem) => {
+        const selectedOptions = elem.parentNode.querySelectorAll(
+            "input:checked"
+        );
+        const selectedValues = [];
+        for (let o of selectedOptions.values()) {
+            selectedValues.push(o.value);
+        }
 
-    [...elem.options].forEach((option) => {
-      if (selectedValues.includes(option.value)) {
-        option.selected = true;
-      } else {
-        option.selected = false;
-      }
-    });
-  };
+        [...elem.options].forEach((option) => {
+            if (selectedValues.includes(option.value)) {
+                option.selected = true;
+            } else {
+                option.selected = false;
+            }
+        });
+    };
 
-  render = (elem) => {
-    // TODO: check if elem is select, if not throw exception
-    elem.style.display = "none";
+    render = (elem) => {
+        // TODO: check if elem is select, if not throw exception
+        elem.style.display = "none";
 
-    const wrapper = document.createElement("div");
-    wrapper.style.position = "relative";
+        const wrapper = document.createElement("div");
+        wrapper.style.position = "relative";
 
-    wrapper.classList.add(...elem.classList);
+        wrapper.classList.add(...elem.classList);
 
-    const dropdown = document.createElement("div");
-    dropdown.textContent = elem.getAttribute("placeholder");
+        const dropdown = document.createElement("div");
+        dropdown.textContent = elem.getAttribute("placeholder");
 
-    wrapper.append(dropdown);
+        wrapper.append(dropdown);
 
-    // TODO: elem.children.length > 0 else throw exception
+        // TODO: elem.children.length > 0 else throw exception
 
-    const items = [];
-    [...elem.options].forEach((option) => {
-      const item = this.createItems(option);
-      if (!item) return;
-      item.addEventListener("click", (e) => {
-        this.onClickItem(e, item, elem.multiple);
-        this.populateValues(elem);
-      });
+        const items = [];
+        [...elem.options].forEach((option) => {
+            const item = this.createItems(option);
+            if (!item) return;
+            item.addEventListener("click", (e) => {
+                this.onClickItem(e, item, elem.multiple);
+                this.populateValues(elem);
+            });
 
-      return items.push(item);
-    });
+            return items.push(item);
+        });
 
-    const dropdownMenu = document.createElement("ul");
-    dropdownMenu.style.position = "absolute";
-    dropdownMenu.style.display = "none";
-
-    dropdown.addEventListener("click", () => {
-      if (dropdownMenu.style.display === "none") {
-        dropdownMenu.style.display = "block";
-      } else {
+        const dropdownMenu = document.createElement("ul");
+        dropdownMenu.style.position = "absolute";
         dropdownMenu.style.display = "none";
-      }
-    });
 
-    dropdownMenu.append(...items);
+        dropdown.addEventListener("click", () => {
+            if (dropdownMenu.style.display === "none") {
+                dropdownMenu.style.display = "block";
+            } else {
+                dropdownMenu.style.display = "none";
+            }
+        });
 
-    elem.parentNode.insertBefore(wrapper, elem);
-    wrapper.append(elem);
+        dropdownMenu.append(...items);
 
-    wrapper.append(dropdownMenu);
+        elem.parentNode.insertBefore(wrapper, elem);
+        wrapper.append(elem);
 
-    document.addEventListener("click", (e) => {
-      if (!wrapper.contains(e.target) && e.target !== wrapper) {
-        dropdownMenu.style.display = "none";
-      }
-    });
-  };
+        wrapper.append(dropdownMenu);
+
+        document.addEventListener("click", (e) => {
+            if (!wrapper.contains(e.target) && e.target !== wrapper) {
+                dropdownMenu.style.display = "none";
+            }
+        });
+    };
 }
 
 export default CustomSelect;
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,22 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    library: "CustomSelect",
-    libraryTarget: "umd",
-    libraryExport: "default",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      },
-    ],
-  },
+    entry: "./src/index.js",
+    output: {
+        filename: "custom-select.js",
+        path: path.resolve(__dirname, "dist"),
+        library: "CustomSelect",
+        libraryTarget: "umd",
+        libraryExport: "default",
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: "babel-loader",
+            },
+        ],
+    },
 };
